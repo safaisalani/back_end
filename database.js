de6be4bbf57f112a6067f1b7359d5520e6281e70
Arun <arun@lentera.in>
2021-08-31 04:47:58 +0530

    dbchanges

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql2");
 var connection = mysql.createConnection({
-    host: '13.234.40.116',
-    database:'Ltick_live_22',
+    host: 'localhost',
+    database:'crud_db',
     user: 'root',
-    password: 'Lentera@20192'
+    password: '20192'
 })
 
 module.exports = connection;
